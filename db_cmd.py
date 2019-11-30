71203ddf60a4387c5e5991c236e5eb691b602fb7
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2019-11-30 11:35:19 -0600

    Assignments table created successfully; Cascade with foreign key implemented

--- db_cmd.py
+++ db_cmd.py
@@ -20,16 +20,42 @@ def create_main_table(conn):
     conn.commit()
 
 
-def create_grades_table(conn):
-    pass
+def create_course_table(conn, course_name):
+    c = conn.cursor()
+    c.execute(f'''CREATE TABLE IF NOT EXISTS {course_name} (
+        assignment_type text NOT NULL,
+        assignment_weight float NOT NULL,
+        course_id integer NOT NULL,
+        FOREIGN KEY (course_id)
+            REFERENCES Courses (course_id)
+                ON DELETE CASCADE
+    )''')
+    conn.commit()
+
+
+def create_assignment_table(conn):
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS Assignments (
+        assignment_type text NOT NULL,
+        assignment_grade float NOT NULL,
+        course_id integer NOT NULL,
+        FOREIGN KEY (course_id)
+            REFERENCES Courses (course_id)
+                ON DELETE CASCADE
+    )''')
 
 
 def add_course(conn, course_name, semester):
     c = conn.cursor()
     if check_new(conn, course_name):
+        create_course_table(conn, course_name)
         c.execute('INSERT INTO Courses VALUES (?, ?, ?)',
                   [None, course_name, semester])
         conn.commit()
+
+
+def update_course(conn, course_name):
+    pass
 
 
 def check_new(conn, course_name):
--- report-card.py
+++ report-card.py
@@ -2,13 +2,13 @@ import db_cmd as db
 
 database = 'report-card.db'
 conn = db.create_connection(database)
-db.create_main_table(conn)
-db.add_course(conn, 'EE 302H', 'F19')
-conn.close()
 
 
 def main():
-    pass
+    db.create_main_table(conn)
+    db.add_course(conn, 'EE302H', 'F19')
+    db.create_assignment_table(conn)
+    conn.close()
 
 
 if __name__ == '__main__':
